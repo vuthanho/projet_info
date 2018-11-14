863eebe1dd19ad2766ce998bc91e4ba0bb374f81
Antoinerld <43623306+Antoinerld@users.noreply.github.com>
2018-11-14 14:23:32 +0100

    Add files via upload

---
+++ step2/dico/dico_inst2.s
@@ -0,0 +1,32 @@
+31
+ADDI I 3 Reg Reg Imm
+ADD R 3 Reg Reg Reg
+BEQ I 3 Reg Reg Rel
+AND R 3 Reg Reg Reg
+BLEZ I 2 Reg Rel
+BGTZ I 2 Reg Rel
+DIV R 2 Reg Reg
+BNE I 3 Reg Reg Rel
+J J 1 Abs
+JR R 1 Reg
+JAL J 1 Abs
+LUI I 2 Reg Imm
+MFHI R 1 Reg
+LW I 2 Reg Bas
+MULT R 2 Reg Reg
+MFLO R 1 Reg
+OR R 3 Reg Reg Reg
+NOP P 0
+SLL R 3 Reg Reg sa
+ROTR R 3 Reg Reg sa
+SRL R 3 Reg Reg sa
+SLT R 3 Reg Reg Reg
+SW I 2 Reg Bas
+SUB R 3 Reg Reg Reg
+XOR R 3 Reg Reg Reg
+MOVE P 2 Reg Reg
+NEG P 2 Reg Reg
+LI P 2 Reg Imm
+BLT P 3 Reg Reg Abs
+LW P 2 Reg Abs
+SW P 2 Reg Abs
